5549bdf4a85b507ecb2e23a192a70b8a179c576d
Germán López Pineda <me1lopig@uco.es>
2020-01-05 23:07:18 +0100

    Actualizacion para permitir el uso de varios tipos de variables

--- Multiplicar/Multiplicar.cpp
+++ Multiplicar/Multiplicar.cpp
@@ -1,15 +1,15 @@
 // Multiplicar.cpp 
 // definir una funcion para multiplicar dos numeros de cualquier tipo
-//
+// se usa una plantilla con dos tipo de variable de entrada 
 //
 
 #include <iostream>
 
 using namespace std;
 
-
-template<class tipo>
-tipo multi(tipo,tipo); // los dos argumentos deben de ser del mismo tipo
+// prototipo de plantillas a usar
+template<class tipo,class tipo2>
+tipo multi(tipo,tipo2); // se puede colocar varios tipos de class para  cada tipo de variable
 
 
 int main()
@@ -20,10 +20,12 @@ int main()
     int numi1 = 3;
     int numi2 = 6;
 
+    // multiplicacion con flotantes, enteros y mixto
     cout.precision(4);
     cout << "El producto de " << numf1<< " y de " << numf2 << " es " << multi(numf1, numf2)<<endl;
-    cout << "El producto de " << numi1 << " y de " << numi2 << " es " << multi(numi1, numi2) << endl;
-    
+    cout << "El producto de " << numi1 << " y de " << numi2 << " es " << multi(numi1, numi2) << endl;  
+    cout << "El producto de " << numf1 << " y de " << numi2 << " es " << multi(numf1, numi2) << endl;
+
 
     cout << "Pulsa una tecla ";
     cin.get();
@@ -31,8 +33,8 @@ int main()
     return 0;
 }
 
-template<class tipo>
-tipo multi(tipo num1,tipo num2)
+template<class tipo,class tipo2>
+tipo multi(tipo num1,tipo2 num2)
 {
 // producto de dos numeros de cualquier tipo
     return num1 * num2;
