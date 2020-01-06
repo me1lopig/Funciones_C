a3b5079ac3badf223c3a4f8943fd7f7444da3334
Germán López Pineda <me1lopig@uco.es>
2020-01-06 01:00:42 +0100

    Se han inclido nuevas funciones

--- Multiplicar/Multiplicar.cpp
+++ Multiplicar/Multiplicar.cpp
@@ -2,6 +2,7 @@
 // definir una funcion para multiplicar dos numeros de cualquier tipo
 // se usa una plantilla con dos tipo de variable de entrada 
 // se define una funcion para elevar al cuadrado un numero
+//y varias mas
 //
 
 #include <iostream>
@@ -18,29 +19,40 @@ tipo cuad(tipo); // funcion para elevar al cuadrado un numero
 template<class tipo1>
 tipo1 elev(tipo1,int); // funcion para elevar un numero a otro entero
 
+float partefraccionaria(float);
+
 
 int main()
 {
-    float numf1 = 3.14;
-    float numf2 = 5.25;
+    float numf1 = -3.144;
+    float numf2 = 5.255;
 
     int numi1 = 2;
     int numi2 = 3;
 
     // multiplicacion con flotantes, enteros y mixto
     //cout.precision(4);
+    cout << "Producto entre dos numeros" << endl;
     cout << "El producto de " << numf1<< " y de " << numf2 << " es " << multi(numf1, numf2)<<endl;
     cout << "El producto de " << numi1 << " y de " << numi2 << " es " << multi(numi1, numi2) << endl;  
     cout << "El producto de " << numf1 << " y de " << numi2 << " es " << multi(numf1, numi2) << endl;
 
     // cuadrados con la funcion de multiplicar y con la de elevar al cudrado
     
+    cout<<"\nNumero elevado al cuadrado" << endl;
     cout << "Elevacion al cuadrado con la funcion multiplicar de " << numf1 << " es " << multi(numf1, numf1) << endl;
     cout << "Elevacion al cuadrado con la funcion cuad de " << numf1 << " es " << cuad(numf1) << endl;
 
     // valor de un numero elevado a un entero
-
+    cout << "\nNumero elevado a otro" << endl;
     cout << "El numero " << numf1 << " elevado a  " << numi2 << " es " << elev(numf1, numi2) << endl;
+
+    // parte decimal de un numero real
+
+    cout << "\nCalculo de la parte fraccionaria de un numero real" << endl;
+    cout << "La parte decimal de  " << numf1 << " es " << partefraccionaria(numf1) << endl;
+    cout << "La parte decimal de  " << numf2 << " es " << partefraccionaria(numf2) << endl;
+
 
     cout << "Pulsa una tecla para terminar ";
     cin.get();
@@ -65,6 +77,7 @@ tipo cuad(tipo num)
 template<class tipo1>
 tipo1 elev(tipo1 x, int n)
 {
+    // calculo de un numero elevado a otro
     tipo1 cal=1;
 
     for (int i = 0; i < n; i++)
@@ -72,4 +85,12 @@ tipo1 elev(tipo1 x, int n)
         cal =cal*x;
     }
     return cal;
+}
+
+float partefraccionaria(float num)
+{
+    // funcion para obtener a parte decimal de un numero real
+
+    return num - int(num);
+
 }
